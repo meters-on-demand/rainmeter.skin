1c9201b4c4d929c757e55b944ee87ff8489c3efe
reisir <reisirito@gmail.com>
2023-09-30 18:02:47 +0300

    add eslintrc and formatonsave

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,11 @@
+import { Stack } from "@mui/material";
+
+import logo from "../assets/mond.svg";
+
+export default function Navigation(props) {
+  return (
+    <Stack>
+      <img src={logo} />;
+    </Stack>
+  );
+}
