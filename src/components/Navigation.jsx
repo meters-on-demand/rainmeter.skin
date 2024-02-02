31b49c2d231578f73b739b9738aacd556a4ff6a1
reisir <reisirito@gmail.com>
2024-02-02 23:41:51 +0200

    package logo

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,33 +1,37 @@
 import React from "react";
 
-import logo from "../assets/mond.svg";
-import { Avatar, Box, Dropdown, Menu, MenuButton, MenuItem } from "@mui/joy";
+import logo from "../assets/package.svg";
+import {
+  Avatar,
+  Box,
+  Dropdown,
+  Link,
+  Menu,
+  MenuButton,
+  MenuItem,
+} from "@mui/joy";
 
 import bg from "../helpers/bg.js";
 
 export default function Navigation(props) {
   return (
-    <Box
-      sx={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        zIndex: 99999,
-        ...bg,
-      }}
-    >
+    <Box sx={{ zIndex: 99999, ...bg }} className="absolute top-4 left-4">
       <Dropdown>
         <MenuButton sx={{ ...bg, p: 0 }} variant="plain">
-          <Avatar
-            src={logo}
-            sx={{
-              "--Avatar-size": "64px",
-            }}
-            variant="plain"
-          />
+          <Avatar src={logo} size="md" variant="plain" />
         </MenuButton>
-        <Menu>
-          <MenuItem></MenuItem>
+        <Menu placement="bottom-start">
+          <MenuItem>
+            <Link href="https://github.com/meters-on-demand">
+              Meters on Demand
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            <Link href="https://docs.rainmeter.skin">Wiki</Link>
+          </MenuItem>
+          <MenuItem>
+            <Link href="https://rainmeter.net">Get Rainmeter</Link>
+          </MenuItem>
         </Menu>
       </Dropdown>
     </Box>
